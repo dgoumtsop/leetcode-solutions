cfc1836c83e17f32c0eb00ce14aa57b1e8d3f02f
Damaris Goumtsop <117386587+dgoumtsop@users.noreply.github.com>
2025-10-31 18:23:24 -0400

    Update ReverseLinkedList.java

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -1,1 +1,15 @@
+// 
 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode temp = curr.next; // store next
+            curr.next = prev;          // reverse link
+            prev = curr;               // move prev forward
+            curr = temp;               // move curr forward
+        }
+        return prev; // new head
+    }
+}
