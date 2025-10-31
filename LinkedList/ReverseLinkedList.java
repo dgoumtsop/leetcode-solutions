21fffbb5124f0923432474812bbd1bae708989e5
Damaris Goumtsop <117386587+dgoumtsop@users.noreply.github.com>
2025-10-31 18:24:56 -0400

    Update ReverseLinkedList.java

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -1,14 +1,18 @@
-// 
+// Problem: Reverse LinkedList
+// Approach: Two pointers Approach
+// curr and prev pointer, store the next and reverse the link 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode curr = head;
         ListNode prev = null;
         while (curr != null) {
-            ListNode temp = curr.next; // store next
-            curr.next = prev;          // reverse link
-            prev = curr;               // move prev forward
-            curr = temp;               // move curr forward
+            ListNode temp = curr.next; 
+            curr.next = prev;          
+            prev = curr;              
+            curr = temp;            
         }
         return prev; // new head
     }
