4d100dbed5f137776096db37a54f5fe3c551064a
Damaris Goumtsop <117386587+dgoumtsop@users.noreply.github.com>
2026-03-01 19:22:26 -0500

    Update ContainsDuplicate.java

--- arrays/ContainsDuplicate.java
+++ arrays/ContainsDuplicate.java
@@ -10,12 +10,11 @@ import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
      Set<Integer> set = new HashSet<>();
-     for(int num : nums){
-        if(set.contains(num)){
-            return true;
-        }
-        set.add(num);
-     }
-     return false;
-    }
+   for(int num : nums){
+       if(!set.add(num){
+           return true;
+       }
+   }
+       return false;
+   }
 }
