49928d1a73f2745868f1df3138aa2a94c9960a3c
Damaris Goumtsop <117386587+dgoumtsop@users.noreply.github.com>
2026-03-11 19:51:41 -0400

    Create GroupAnagrams.java

---
+++ arrays/GroupAnagrams.java
@@ -0,0 +1,20 @@
+**
+ * Group Anagrams - Sorting Solution
+ * 
+ * Time: O(m * n log n)
+ * Space: O(m)  HashMap stores m strings
+ */
+   class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+            for(String word : strs){
+                char[] chars = word.toCharArray();
+                Arrays.sort(chars);
+
+                String key = new String(chars);
+                map.putIfAbsent(key, new ArrayList<>());
+                map.get(key).add(word);
+            }
+            return new ArrayList<>(map.values());
+    }
+}
